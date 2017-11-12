e7f7f1da2cc1c64bdbb2eda2faea43dd354ef3b2
Debashis Paul <send2deb@gmail.com>
2017-11-12 19:12:17 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -0,0 +1,60 @@
+package com.example.android.sunshine;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecast;
+    private TextView mWeatherDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        mWeatherDisplay = (TextView) findViewById(R.id.tv_display_weather);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if (intentThatStartedThisActivity != null) {
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+                mForecast = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mWeatherDisplay.setText(mForecast);
+            }
+        }
+    }
+
+    // COMPLETED (3) Create a menu with an item with id of action_share
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    // COMPLETED (4) Display the menu and implement the forecast sharing functionality
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            ShareCompat.IntentBuilder.from(this)
+                    .setChooserTitle("Share Weather Forecast")
+                    .setType("text/plain")
+                    .setText(mForecast + "-" + FORECAST_SHARE_HASHTAG)
+                    .startChooser();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
